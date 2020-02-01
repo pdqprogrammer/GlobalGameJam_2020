c3d9a11c79afc6aa41490721d7440b587bcfd285
pdqprogrammer <pauldquinones@gmail.com>
2020-01-31 22:00:30 -0600

    Script and Sandbox
    Initial Script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for user inputs and adjust
+
+    }
+
+    private void PlayerJump()
+    {
+
+    }
+
+    private void PlayerMove()
+    {
+
+    }
+
+    private void PlayerGrab()
+    {
+
+    }
+}
