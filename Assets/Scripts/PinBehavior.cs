6621c9e4a3337bfb39ff0929cb9ced3f0c279706
Kristoffer Castaldo <k.castaldo95@gmail.com>
2020-02-01 16:49:13 -0600

    Added animations and functionalilty to pins and springs.

---
+++ Assets/Scripts/PinBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinBehavior : MonoBehaviour
+{
+    //States curState;
+
+    public Animator animator;
+
+    //enum States { Rigid, Bounce, Locked }
+
+    [SerializeField]
+    GameObject spring;
+
+    private void Start()
+    {
+        //curState = States.Rigid;
+
+        animator = this.gameObject.GetComponent<Animator>();
+        //Bounce();
+        //animator.Play("Pin_Static");
+
+    }
+
+    //called in SpringBehavior ln 26
+    public void dothething(GameObject _spring)
+    {
+        _spring = spring;
+       // if (curState == States.Rigid)
+       //     Bounce();
+    }
+
+    //set to do animation
+    public void Bounce()
+    {
+        Debug.Log("Bouncing");
+        animator.SetBool("Bounce", true);
+        //curState = States.Bounce;
+        //gameObject.GetComponent<BoxCollider>().enabled = true;
+        animator.Play("Pin_Bounce");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            LockThis();
+        }
+    }
+
+    private void LockThis()
+    {
+        Debug.Log("Locked");
+        //curState = States.Locked;
+        animator.SetBool("Bounce", false);
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        Rigidbody rig = gameObject.GetComponent<Rigidbody>();
+        rig.constraints = RigidbodyConstraints.FreezeAll;
+        //spring.GetComponent<MeshRenderer>().enabled = false;
+        //animator.SetBool("Bounce", false);
+        //animator.Play("Pin_Fall");
+    }
+
+
+}
---
+++ Assets/Scripts/SpringBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBehavior : MonoBehaviour
+{
+    GameObject pin;
+    GameObject Spring;
+    [SerializeField]
+    GameObject hiddenPin;
+
+    private void Start()
+    {
+        Spring = this.gameObject;
+        //hiddenPin = transform.GetChild(0).gameObject;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collider name is " + other);
+        if (other.gameObject.tag == "PullableObject")
+        {
+            Debug.Log("Setting Pin");
+            pin = other.gameObject;
+            Debug.Log("toggle spring collider");
+            this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
+            Debug.Log("do the thing");
+            //pin.GetComponent<PinBehavior>().dothething(Spring);
+            Destroy(pin);
+            hiddenPin.SetActive(true);
+            hiddenPin.GetComponent<PinBehavior>().Bounce();
+        }
+    }
+
+
+}
