4c84d08993b819eb4e8e5c5065a55f32c0ed2adf
Kristoffer Castaldo <k.castaldo95@gmail.com>
2020-02-01 21:25:26 -0600

    Menu functionallity

---
+++ Assets/Scripts/ButtonFunction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunction : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource source;
+    [SerializeField]
+    AudioClip[] otherClip;
+
+    public void TimeToStop()
+    {
+        source.volume = 0.3f;
+        source.clip = otherClip[2];
+        source.Play();
+    }
+
+    public void PizzaTime()
+    {
+        source.volume = 0.3f;
+        source.clip = otherClip[1];
+        source.Play();
+    }
+
+    //load level
+
+    //quit
+    //credits
+    
+}
