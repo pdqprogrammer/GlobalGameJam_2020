d1e52d5580bc2d84a1ad113c304ff729e62acdc2
pdqprogrammer <pauldquinones@gmail.com>
2020-02-02 12:42:20 -0600

    Enemy Rotation Fix
    Fix for enemy rotation

--- Assets/Scripts/EnemyMovementScript.cs
+++ Assets/Scripts/EnemyMovementScript.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class EnemyMovementScript : MonoBehaviour
 {
     public float enemySpeed = 3.0f;
+    public float turnSpeed = 1.0f;
     private float defaultY;
 
     public GameObject[] MovePoints;
@@ -31,8 +32,12 @@ public class EnemyMovementScript : MonoBehaviour
 
         transform.position = movementVector;
 
-        transform.LookAt(MoveTowardObject.transform);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(MoveTowardObject.transform.position.normalized), 0.05f);
+        //transform.LookAt(MoveTowardObject.transform);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(MoveTowardObject.transform.position.normalized), 0.1f);
+
+        Vector3 relativePos = MoveTowardObject.transform.position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, turnSpeed * Time.deltaTime);
     }
 
     void FixedUpdate()
@@ -51,8 +56,11 @@ public class EnemyMovementScript : MonoBehaviour
 
             MoveTowardObject = MovePoints[currMovePoint];
         }
+    }
 
-        if (other.gameObject.tag.Equals("PullableOBject"))
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("PullableOBject"))
         {
             currMovePoint--;
 
